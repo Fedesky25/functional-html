dbcf1e1c29128990275245699ff3cf49faf25052
Fedesky25 <fedesky.guglielmi@gmail.com>
2022-09-16 13:41:20 +0200

    test: each

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-import test from "ava";
-import { render } from "posthtml-render";
-import { HTMLComponent, componentify } from "../dist/index.js";
+const test = require("ava").default;
+const { render } = require("posthtml-render");
+const { createComponentFrom, componentify } = require("../dist/index.js");
 
 test("throws on invalid path", async i => {
     const c = componentify("test/unexistant.html");
@@ -19,51 +19,43 @@ test("throws on empty template", async i => {
 
 test("component renders", async i => {
     const c = await componentify("test/Simple.html");
-    i.truthy(c.render());
+    i.truthy(c({},{}));
 });
 
 test("conditional: basics", async i => {
     const c = await componentify("test/conditional/basic.html");
-    i.is(render(c.render({flag: true})), "true");
-    i.is(render(c.render({flag: false})), "false");
+    i.is(render(c({flag: true}, {})), "true");
+    i.is(render(c({flag: false}, {})), "false");
 });
 
 test("conditional: cases after default are skipped", async i => {
     const c = await componentify("test/conditional/skip-after-def.html");
-    i.is(render(c.render({value: 2})), "default");
+    i.is(render(c({value: 2},{})), "default");
 });
 
-test("conditional: wrong content", i => i.notThrowsAsync(() => componentify("test/conditional/wrong-content.html")));
+test("conditional: wrong content", i => i.throwsAsync(() => componentify("test/conditional/wrong-content.html")));
 
-test("conditional: no cases", i => i.notThrowsAsync(() => componentify("test/conditional/no-cases.html")));
+test("conditional: no cases", i => i.throwsAsync(() => componentify("test/conditional/no-cases.html")));
 
 test("conditional: empty condition", i => i.throwsAsync(() => componentify("test/conditional/empty-condition.html")));
 
-
-test("each item", async i => {
-    const comp = new HTMLComponent(`<link rel="attr" title="input">
-<template>
-    <each item in="input">
-        \${item}
-    </conditional>
-</template>    
-`, ".", "conditional");
-    await comp.ready;
-    const res = comp.render({input: [1,2,3]});
-    const text = render(res);
-    return i.is(text, "123");
+test("each: empty", async i => {
+    const c = await componentify("test/each/empty.html");
+    i.is(render(c()), "");
 });
 
-test("each item and index", async i => {
-    const comp = new HTMLComponent(`<link rel="attr" title="input">
-<template>
-    <each item index in="input">
-        \${index}: \${item},
-    </conditional>
-</template>    
-`, ".", "conditional");
-    await comp.ready;
-    const res = comp.render({input: ['a','b','c']});
-    const text = render(res);
-    return i.is(text, "0: a,1: b,2: c,");
+test("each: no item", async i => {
+    const c = await componentify("test/each/no-item.html");
+    i.is(render(c()), "");
 });
+
+test("each: only index", async i => {
+    const c = await componentify("test/each/only-index.html");
+    i.is(render(c()), "012");
+});
+
+test("each: not-array", async i => {
+    const c = await componentify("test/each/not-array.html");
+    i.throws(() => c());
+});
+
